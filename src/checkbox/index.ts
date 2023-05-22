8abd9f58feaaa196c32827adfdd610756f8fa04e
野迂迂 <yangwei1@outlook.com>
2023-05-22 20:26:29 +0800

    feat: add checkbox

---
+++ src/checkbox/index.ts
@@ -0,0 +1,11 @@
+import { fireEvent } from "@testing-library/react";
+import { IContainer } from "src/interface";
+import { getProvider } from "src/provider";
+
+const { prefixCls } = getProvider();
+
+export function fireChange(container: IContainer, index: number) {
+  const ele = container.querySelectorAll(`.${prefixCls}-checkbox-input`);
+  if (!ele.item(index)) return;
+  fireEvent.click(ele.item(index));
+}
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * as input from "./input";
 export * as inputNumber from "./inputNumber";
 export * as select from "./select";
 export * as dropdown from "./dropdown";
+export * as checkbox from "./checkbox";
 export { getProvider, provider } from "./provider";
